5d87f6d5ba5bceeeb7f36a808fbda1dbdff0ed02
Nick Kornek <nkornek@gmail.com>
2014-11-08 15:40:15 -0500

    replaced files

---
+++ Bosspants Unity/Assets/Scripts/Character_movement.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character_movement : MonoBehaviour {
+	public float minspeed, maxSpeed, moveSpeed, jumpForce;
+	public bool jumping;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//movement
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			transform.Translate(Vector2.right * -moveSpeed * Time.deltaTime, Space.Self);
+		}
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			transform.Translate(Vector2.right * moveSpeed * Time.deltaTime, Space.Self);
+		}
+		if (Input.GetKey(KeyCode.A))
+		{
+			rigidbody2D.AddForce(Vector2.up * jumpForce);
+			jumping = true;
+		}
+	
+	}
+
+	void ControlSpeed() {
+		if (moveSpeed < maxSpeed)
+		{
+			moveSpeed += maxSpeed/30;
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		if (coll.transform.tag == "Ground")
+		{
+			jumping = false;
+		}
+	}
+}
