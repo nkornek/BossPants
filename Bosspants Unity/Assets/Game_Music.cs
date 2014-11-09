dd9b7e604d6ad3910b9b14a320a88be149b82f74
Nick Kornek <nkornek@gmail.com>
2014-11-09 15:32:32 -0500

    Magical Music OMG

---
+++ Bosspants Unity/Assets/Game_Music.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Game_Music : MonoBehaviour {
+	public AudioClip mainMusic, bossMusic;
+
+	// Use this for initialization
+	void Start () {
+		DontDestroyOnLoad (gameObject);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
