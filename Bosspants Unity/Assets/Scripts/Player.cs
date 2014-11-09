28e6b64558e7a494f0db00e23177076615021a33
Nick Kornek <nkornek@gmail.com>
2014-11-09 01:07:25 -0500

    Working on animations for Pants 2

--- Bosspants Unity/Assets/Scripts/Player.cs
+++ Bosspants Unity/Assets/Scripts/Player.cs
@@ -5,19 +5,38 @@ public class Player : MonoBehaviour {
 
 	public float triggerDistance;
 	public int peoplePantsed, peopleToPants;
+	public float chargeTime, chargeTotal;
 
 	// Use this for initialization
 	void Start () {
-
+		chargeTime = chargeTotal;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+	//trigger nearby peeps
 		if (Input.GetKey(KeyCode.Space))
 		{
 			CallPeople();
 		}
+	//kick!
+		if (Input.GetKey(KeyCode.S))
+		{
+			chargeTime -= Time.deltaTime;
+		}
+		if (Input.GetKeyUp(KeyCode.S))
+		{
+			if (chargeTime <= 0)
+			{
+				//bicycle kick
+			}
+			else
+			{
+				//regular kick
+			}
+			chargeTime = chargeTotal;
+		}
+	
 	}
 
 	public void addPerson ()
