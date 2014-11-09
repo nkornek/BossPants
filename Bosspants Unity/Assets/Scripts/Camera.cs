c3bae653586fabdd67e80600c2c46282e15ffdf7
Louis Louis <louis.sciaannamblo@gmail.com>
2014-11-08 21:12:35 -0500

    fartcakes 4 the final fartcake

---
+++ Bosspants Unity/Assets/Scripts/Camera.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera : MonoBehaviour {
+
+	public GameObject player;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position= new Vector3(transform.position.x, player.transform.position.y,transform.position.z);
+//		transform.position.y= player.transform.position.y;
+	}
+}
