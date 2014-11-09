bc326ae5f3f2cb1b698582533050fcea18b12e4a
Louis Louis <louis.sciaannamblo@gmail.com>
2014-11-09 15:02:19 -0500

    i am making a serious commitment

---
+++ Bosspants Unity/Assets/Scripts/Audiosource.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Audiosource : MonoBehaviour {
+
+	public AudioClip [] clips;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
